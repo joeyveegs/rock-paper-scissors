02db248a049dcfb1fcee4e41798f2a45c949dff2
joeyveegs <64642147+joeyveegs@users.noreply.github.com>
2024-10-02 10:43:05 +0100

    Write logic to get human choice

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 function getComputerChoice() {
-    choice = Math.random()
-    if (choice < 0.33) {
+    compChoice = Math.random()
+    if (compChoice < 0.33) {
         return console.log("rock")
     }
-    else if (choice > 0.33 && choice < 0.66) {
+    else if (compChoice > 0.33 && choice < 0.66) {
         return console.log("paper")
     }
-    else
+    else {
         return console.log("scissors");
+    }
+}
+
+function getHumanChoice() {
+    let playerChoice = prompt("Rock, paper or scissors?\nEnter your choice and press OK:");
+    if (playerChoice === null) {
+        return
+    }
+    else if (playerChoice.toLowerCase() == "rock") {
+        return console.log(playerChoice);
+    }
+    else if (playerChoice.toLowerCase() == "paper") {
+        return console.log(playerChoice);
+    }
+    else if (playerChoice.toLowerCase() == "scissors") {
+        return console.log(playerChoice);
+    }
+    else {
+        return invalidPlayerChoice()
+    }
+}
+
+function invalidPlayerChoice() {
+    alert("Whoops! That didn't work.\nTry entering your choice again...")
+    return getHumanChoice()
 }
