63efa04eec0e47a9083809c506bf466c0baa998a
joeyveegs <64642147+joeyveegs@users.noreply.github.com>
2024-10-02 17:44:22 +0100

    Create working playRound function without score counters

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function getComputerChoice() {
-    let computerChoice = Math.random()
-    if (computerChoice < 0.33) {
+    let compInput = Math.random()
+    if (compInput < 0.33) {
         return "rock"
     }
-    else if (computerChoice > 0.33 && computerChoice < 0.66) {
+    else if (compInput > 0.33 && compInput < 0.66) {
         return "paper"
     }
     else {
@@ -12,28 +12,46 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Rock, paper or scissors?\nEnter your choice and press OK:")
-    if (humanChoice === null) {
+    let userInput = prompt("Rock, paper or scissors?\nEnter your choice and press OK:")
+    if (userInput === null) {
         return
     }
-    else if (humanChoice.toLowerCase() == "rock") {
+    else if (userInput.toLowerCase() == "rock") {
         return "rock"
     }
-    else if (humanChoice.toLowerCase() == "paper") {
+    else if (userInput.toLowerCase() == "paper") {
         return "paper"
     }
-    else if (humanChoice.toLowerCase() == "scissors") {
+    else if (userInput.toLowerCase() == "scissors") {
         return "scissors"
     }
     else {
-        return invalidhumanChoice()
+        return invalidUserInput()
     }
 }
 
-function invalidhumanChoice() {
+function invalidUserInput() {
     alert("Whoops! That didn't work.\nTry entering your choice again...")
     return getHumanChoice()
 }
 
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound() {
+    let humanChoice = getHumanChoice()
+    let computerChoice = getComputerChoice()
+    if (humanChoice == computerChoice) {
+        return `It's a tie! You both chose ${computerChoice}`
+    }
+    else if ((humanChoice == "scissors" && computerChoice == "rock")
+        || (humanChoice == "rock" && computerChoice == "paper")
+        || (humanChoice == "paper" && computerChoice == "scissors")) {
+            return `You lost! The computer chose ${computerChoice}`
+    }
+    else if ((humanChoice == "paper" && computerChoice == "rock")
+        || (humanChoice == "scissors" && computerChoice == "paper")
+        || (humanChoice == "rock" && computerChoice == "scissors")) {
+            return `You won! The computer chose ${computerChoice}`
+    }
+}
